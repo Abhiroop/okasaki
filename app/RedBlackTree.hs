bb1d6ef62cd0b27d42d63977d39138beee6a7095
Abhiroop Sarkar <asiamgenius@gmail.com>
2017-10-23 00:45:14 +0100

    The balance function handling the four dangerous cases

--- app/RedBlackTree.hs
+++ app/RedBlackTree.hs
@@ -32,7 +32,10 @@ insert x s = makeBlack $ ins s
         makeBlack (T _ a y b) = T B a y b
 
 balance :: Color -> Set a -> a -> Set a -> Set a
-balance = undefined
-
+balance B (T R (T R a x b) y c) z d = T R (T B a x b) y (T B c z d)
+balance B (T R a x (T R b y c)) z d = T R (T B a x b) y (T B c z d)
+balance B a x (T R (T R b y c) z d) = T R (T B a x b) y (T B c z d)
+balance B a x (T R b y (T R c z d)) = T R (T B a x b) y (T B c z d)
+balance color a x b = T color a x b
 
 
