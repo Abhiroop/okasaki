5e2cb3eddb6b6655adc09a2d0d3f208ef6c4d5c3
Abhiroop Sarkar <asiamgenius@gmail.com>
2017-10-22 23:23:46 +0100

    The skeleton of the red black tree

--- app/RedBlackTree.hs
+++ app/RedBlackTree.hs
@@ -8,7 +8,7 @@ data Tree a = E | T Color (Tree a) a (Tree a)
 -- 1. No red node has a red parent
 -- 2. Every path from the root node to an empty node contains the same number of black nodes
 
--- Simple Set operations
+-- | Simple Set operations
 type Set a = Tree a
 
 empty :: Set a
@@ -20,3 +20,19 @@ member x (T _ a y b)
   | x < y     = member x a
   | x == y    = True
   | otherwise = member x b
+
+-- | Insertion
+insert :: (Ord a) => a -> Set a -> Set a
+insert x s = makeBlack $ ins s
+  where ins E  = T R E x E
+        ins (T color a y b)
+          | x < y  = balance color (ins a) y b
+          | x == y = T color a y b
+          | x > y  = balance color a y (ins b)
+        makeBlack (T _ a y b) = T B a y b
+
+balance :: Color -> Set a -> a -> Set a -> Set a
+balance = undefined
+
+
+
