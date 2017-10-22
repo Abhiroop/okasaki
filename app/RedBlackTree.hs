f342b27df356eb5115f1251749f03985f598f11d
Abhiroop Sarkar <asiamgenius@gmail.com>
2017-10-22 22:56:07 +0100

    The invariants

--- app/RedBlackTree.hs
+++ app/RedBlackTree.hs
@@ -3,3 +3,9 @@ module RedBlackTree where
 data Color = R | B
 
 data Tree a = E | T Color (Tree a) a (Tree a)
+
+-- Invariants
+-- 1. No red node has a red parent
+-- 2. Every path from the root node to an empty node contains the same number of black nodes
+
+
