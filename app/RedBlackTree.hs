6c49d0d52eaf2de3e017514baf0074f799535493
Abhiroop Sarkar <asiamgenius@gmail.com>
2017-10-22 23:00:55 +0100

    Simple set operations

--- app/RedBlackTree.hs
+++ app/RedBlackTree.hs
@@ -8,4 +8,15 @@ data Tree a = E | T Color (Tree a) a (Tree a)
 -- 1. No red node has a red parent
 -- 2. Every path from the root node to an empty node contains the same number of black nodes
 
+-- Simple Set operations
+type Set a = Tree a
 
+empty :: Set a
+empty = E
+
+member :: (Ord a) => a -> Set a -> Bool
+member x E    = False
+member x (T _ a y b)
+  | x < y     = member x a
+  | x == y    = True
+  | otherwise = member x b
