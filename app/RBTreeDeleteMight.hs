44f172dec4c0fd4ebf6b4197bf1af1d6da21a040
Abhiroop Sarkar <asiamgenius@gmail.com>
2017-10-27 15:23:16 +0100

    First version of the double black deletion

--- app/RBTreeDeleteMight.hs
+++ app/RBTreeDeleteMight.hs
@@ -19,7 +19,12 @@ lighten NB = error "Can't lighten further"
 blackenRoot :: Tree a -> Tree a
 blackenRoot (T _ a x b) = T B a x b
 
+reddenTree :: Tree a -> Tree a
+reddenTree (T c l x r) = T (lighten c) l x r
 
+maxTree :: Tree a -> a
+maxTree (T _ _ x E) = x
+maxTree (T _ _ x r) = maxTree r
 
 delete :: (Ord a) => a -> Tree a -> Tree a
 delete x s = blackenRoot (del x s)
@@ -31,6 +36,48 @@ del x s@(T color a y b)
   | otherwise = remove s
 
 
-remove = undefined
+remove :: Tree a -> Tree a
+remove (T R E _ E) = E
+remove (T B E _ E) = E
+remove (T B E _ (T R a x b)) = T B a x b
+remove (T B (T R a x b) _ E) = T B a x b
+remove (T color l y r) = bubble color l' mx r
+ where mx = maxTree l
+       l' = removeMax l
 
-bubble = undefined
+removeMax :: Tree a -> Tree a
+removeMax s@(T _ _ _ E) = remove s
+removeMax s@(T color l x r) = bubble color l x (removeMax r)
+
+isBB :: Tree a -> Bool
+isBB (T BB _ _ _) = True
+isBB _ = False
+
+bubble :: Color -> Tree a -> a -> Tree a -> Tree a
+bubble color l x r
+ | isBB(l) || isBB(r) = balance (blacken color) (reddenTree l) x (reddenTree r)
+ | otherwise          = balance color l x r
+
+balance :: Color -> Tree a -> a -> Tree a -> Tree a
+-- Okasaki's insertion cases
+balance B (T R (T R a x b) y c) z d = T R (T B a x b) y (T B c z d)
+balance B (T R a x (T R b y c)) z d = T R (T B a x b) y (T B c z d)
+balance B a x (T R (T R b y c) z d) = T R (T B a x b) y (T B c z d)
+balance B a x (T R b y (T R c z d)) = T R (T B a x b) y (T B c z d)
+
+-- Matt Might's deletion cases
+balance BB (T R (T R a x b) y c) z d = T B (T B a x b) y (T B c z d)
+balance BB (T R a x (T R b y c)) z d = T B (T B a x b) y (T B c z d)
+balance BB a x (T R (T R b y c) z d) = T B (T B a x b) y (T B c z d)
+balance BB a x (T R b y (T R c z d)) = T B (T B a x b) y (T B c z d)
+
+-- Matt Might's negative black cases
+balance BB a x (T NB (T B b y c) z (T B d w e))
+    = T B (T B a x b) y (balance B c z (T R d w e))
+balance BB (T NB (T B a' w b') x (T B b y c)) z d
+    = T B (balance B (T R a' w b') x b) y (T B c z d)
+
+-- General cases of balancing
+balance color a x b = T color a x b
+
+
