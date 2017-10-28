d2bf5e41a3d6a206dfafde6732c37d9cbbdcd76a
Abhiroop Sarkar <asiamgenius@gmail.com>
2017-10-28 20:32:48 +0100

    Set -> Tree

--- app/RedBlackTree.hs
+++ app/RedBlackTree.hs
@@ -1,6 +1,6 @@
 module RedBlackTree
   (
-    Set,
+    Tree,
     empty,
     member,
     insert)
@@ -15,13 +15,12 @@ data Tree a = E | T Color (Tree a) a (Tree a) deriving Show
 -- 2. Every path from the root node to an empty node contains the same number of black nodes
 -- 3. The root and leaves of the tree are black
 
--- | Simple Set operations
-type Set a = Tree a
+-- | Simple Tree operations
 
-empty :: Set a
+empty :: Tree a
 empty = E
 
-member :: (Ord a) => a -> Set a -> Bool
+member :: (Ord a) => a -> Tree a -> Bool
 member x E    = False
 member x (T _ a y b)
   | x < y     = member x a
@@ -29,7 +28,7 @@ member x (T _ a y b)
   | otherwise = member x b
 
 -- | Insertion
-insert :: (Ord a) => a -> Set a -> Set a
+insert :: (Ord a) => a -> Tree a -> Tree a
 insert x s = makeBlack $ ins s
   where ins E  = T R E x E
         ins (T color a y b)
@@ -38,7 +37,7 @@ insert x s = makeBlack $ ins s
           | x > y  = balance color a y (ins b)
         makeBlack (T _ a y b) = T B a y b
 
-balance :: Color -> Set a -> a -> Set a -> Set a
+balance :: Color -> Tree a -> a -> Tree a -> Tree a
 balance B (T R (T R a x b) y c) z d = T R (T B a x b) y (T B c z d)
 balance B (T R a x (T R b y c)) z d = T R (T B a x b) y (T B c z d)
 balance B a x (T R (T R b y c) z d) = T R (T B a x b) y (T B c z d)
@@ -46,17 +45,17 @@ balance B a x (T R b y (T R c z d)) = T R (T B a x b) y (T B c z d)
 balance color a x b = T color a x b
 
 -- This is simple helper to reduce typing
-balance' :: Set a -> Set a
+balance' :: Tree a -> Tree a
 balance' (T color left value right) = balance color left value right
 
 -- | Deletion
-delete :: (Ord a) => a -> Set a -> Set a
+delete :: (Ord a) => a -> Tree a -> Tree a
 delete x t = makeBlack $ del x t
   where makeBlack (T _ a x b) = T B a x b
         makeBlack E           = E
 
 -- Delete with consecutive red nodes at the top which is rectified in delete
-del :: (Ord a) => a -> Set a -> Set a
+del :: (Ord a) => a -> Tree a -> Tree a
 del x t@(T _ l y r)
   | x < y = delL x t
   | x > y = delR x t
@@ -66,11 +65,11 @@ del x t@(T _ l y r)
 -- We have 2 options
 -- 1. If it(the ccurent node i.e t) is black delelte from t1 and then balance.
 -- 2. If it is red delete from t1 and no need to balance because there are no cases
-delL :: (Ord a) => a -> Set a -> Set a
+delL :: (Ord a) => a -> Tree a -> Tree a
 delL x t@(T B t1 y t2) = balL $ T B (del x t1) y t2
 delL x t@(T R t1 y t2) = T R (del x t1) y t2
 
-balL :: Set a -> Set a
+balL :: Tree a -> Tree a
 balL (T B (T R t1 x t2) y t3) = T R (T B t1 x t2) y t3
 balL (T B t1 y (T B t2 z t3)) = balance' (T B t1 y (T R t2 z t3))      -- t1 root is black in both the last cases because red is handled at the top
 balL (T B t1 y (T R (T B t2 u t3) z t4@(T B l value r))) =
@@ -81,17 +80,17 @@ balL (T B t1 y (T R (T B t2 u t3) z t4@(T B l value r))) =
 -- We have 2 options
 -- 1. If it(the ccurent node i.e t) is black delelte from t2 and then balance.
 -- 2. If it is red delete from t2 and no need to balance because there are no cases
-delR :: (Ord a) => a -> Set a -> Set a
+delR :: (Ord a) => a -> Tree a -> Tree a
 delR x t@(T B t1 y t2) = balR $ T B t1 y (del x t2)
 delR x t@(T R t1 y t2) = T R t1 y (del x t2)
 
-balR :: Set a -> Set a
+balR :: Tree a -> Tree a
 balR (T B t1 y (T R t2 x t3)) = T R t1 y (T B t2 x t3)
 balR (T B (T B t1 z t2) y t3) = balance' (T B (T R t1 z t2) y t3)       -- t3 root is black in both the last cases because red is handled at the top
 balR (T B (T R t1@(T B l value r) z (T B t2 u t3)) y t4) =
   T R (balance' (T B (T R l value r) z t2)) u (T B t3 y t4)
 
-fuse :: Set a -> Set a -> Set a
+fuse :: Tree a -> Tree a -> Tree a
 fuse E t = t
 fuse t E = t
 fuse t1@(T B _ _ _) (T R t3 y t4) = T R (fuse t1 t3) y t4
