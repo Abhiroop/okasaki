c974e0d132fa3906298d387c21dd0fb6efee7305
Abhiroop Sarkar <asiamgenius@gmail.com>
2017-12-08 11:31:18 +0000

    Add all the basic operations to the queue

--- app/Queue.hs
+++ app/Queue.hs
@@ -1,6 +1,26 @@
 module Queue where
 
-data Queue a = Queue [a] [a]
+import Prelude hiding (head)
+
+data Queue a = Queue [a] [a] deriving Show
 
 empty :: Queue a
-empty = undefined
+empty = Queue [] []
+
+isEmpty :: Queue a -> Bool
+isEmpty (Queue [] []) = True
+isEmpty _ = False
+
+snoc :: a -> Queue a -> Queue a
+snoc x (Queue front rear) = Queue front (x:rear)
+
+head :: Queue a -> Maybe a
+head (Queue [] []) = Nothing
+head (Queue [] rear) = head (Queue (reverse rear) [])
+head (Queue (x:xs) rear) = Just x
+
+
+extract :: Queue a -> (Maybe a, Queue a)
+extract q@(Queue [] []) = (Nothing, q)
+extract (Queue [] rear) = extract (Queue (reverse rear) [])
+extract (Queue (x:xs) rear) = (Just x , Queue xs rear)
